baf622ce34d099133b9c90f452c6a7320543ca02
Miles <miles.j.hoo@gmail.com>
2023-04-15 10:29:05 -0500

    add json

--- src/main/java/com/se6387/ahem/controller/MainController.java
+++ src/main/java/com/se6387/ahem/controller/MainController.java
@@ -2,15 +2,21 @@ package com.se6387.ahem.controller;
 
 import com.se6387.ahem.model.CapturedPollutant;
 import com.se6387.ahem.repository.CapturedPollutantRepository;
+import com.se6387.ahem.sensor.AqiMeasurement;
+import com.se6387.ahem.sensor.AqiPolygon;
+import com.se6387.ahem.sensor.AqiPolygons;
 import com.se6387.ahem.service.MapService;
+import com.se6387.ahem.service.Pollutant;
 import com.se6387.ahem.view.Coordinate;
 import com.se6387.ahem.view.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -48,6 +54,33 @@ public class MainController {
         ResponseEntity<List<CapturedPollutant>> responseEntity = new ResponseEntity<>();
         List<CapturedPollutant> byDatetimeBetween = capturedPollutantRepository.findByDatetimeBetween(new Date(startTimestamp), new Date(endTimestamp));
         return responseEntity.success(byDatetimeBetween);
+    }
+
+    @GetMapping("/v1/visualization/polygons")
+    public ResponseEntity<AqiPolygons> poligons(@RequestParam String boundary) {
+        ResponseEntity responseEntity = new ResponseEntity();
+        if (boundary == null || boundary.trim().length() == 0) {
+            return responseEntity.fail("Parameter error");
+        }
+        String[] split = boundary.split(",");
+        double northernLatitude = Double.parseDouble(split[0]);
+        double southernLatitude = Double.parseDouble(split[1]);
+        double westernLongitude = Double.parseDouble(split[2]);
+        double easternLongitude = Double.parseDouble(split[3]);
+
+        AqiMeasurement PM25 = new AqiMeasurement(Pollutant.PM25, 10, Instant.now());
+        AqiMeasurement PM10 = new AqiMeasurement(Pollutant.PM10, 100, Instant.now());
+        AqiMeasurement O3 = new AqiMeasurement(Pollutant.O3, 200, Instant.now());
+        AqiMeasurement SO2 = new AqiMeasurement(Pollutant.SO2, 300, Instant.now());
+        AqiMeasurement CO = new AqiMeasurement(Pollutant.CO, 400, Instant.now());
+        AqiMeasurement NO2 = new AqiMeasurement(Pollutant.NO2, 500, Instant.now());
+        List<AqiMeasurement> aqiMeasurements = new ArrayList<>(Arrays.asList(PM25, PM10, O3, SO2, CO, NO2));
+        AqiPolygon ap = new AqiPolygon(northernLatitude, southernLatitude, westernLongitude, easternLongitude, aqiMeasurements);
+        List<AqiPolygon> list = new ArrayList<>();
+        list.add(ap);
+        AqiPolygons aqiPolygons = new AqiPolygons(list);
+        return responseEntity.success(aqiPolygons);
+
     }
 
 }
