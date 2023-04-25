f939d68afbd5c82b34983a93b9f8f038c9adcc98
jtsmith-utdallas <jamie.smith@utdallas.edu>
2023-04-25 14:30:48 -0500

    added v1/db_tools/reset_data service to clear and repopulate the db with sensors and 12 hours of future data
    add v1/pollutant/get_aqi_polygons service for 3 decimal places
    
    add v1/pollutant/get_aqi_point service  This needs refinement.

--- src/main/java/com/se6387/ahem/repository/SensorRepository.java
+++ src/main/java/com/se6387/ahem/repository/SensorRepository.java
@@ -1,9 +1,22 @@
 package com.se6387.ahem.repository;
 
+import com.se6387.ahem.model.CapturedPollutant;
 import com.se6387.ahem.model.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface SensorRepository extends JpaRepository<Sensor, Integer> {
+
+    @Query("select s from Sensor s where s.radiusMeters<0 and s.latitude <= :nLat and s.latitude >= :sLat and s.longitude >= :wLon and s.longitude <= :eLon")
+    List<Sensor> boxOfSensors(@Param("nLat") BigDecimal nLat, @Param("sLat") BigDecimal sLat,
+                              @Param("wLon") BigDecimal wLon, @Param("eLon") BigDecimal eLon);
+
+
 }
